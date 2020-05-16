8e659a4a2fb6fcf26a8d06eb39307b8c52a6392f
burgas275 <burgas275@gmail.com>
2020-05-16 11:43:06 +0300

    Add project files.

---
+++ Db2CsvBackuper/DBDumper.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.IO
+Imports System.Text
+
+Public Class DBDumper
+    Public Shared Sub DumpTableToFile(ByVal Connection As SqlConnection, ByVal TableName As String, ByVal DestinationFile As String)
+        Using CMD = New SqlCommand("select * from " & TableName, Connection)
+            Using RDR As SqlDataReader = CMD.ExecuteReader()
+                Using OutFile = File.CreateText(DestinationFile)
+                    Dim ColumnNames As String() = GetColumnNames(RDR).ToArray()
+                    Dim NumFields As Integer = ColumnNames.Length
+                    OutFile.WriteLine(String.Join(",", ColumnNames))
+                    If RDR.HasRows Then
+                        While RDR.Read()
+                            Dim ColumnValues As String() = Enumerable.Range(0, NumFields).
+                                Select(Function(i) DumpOneColumn(i, RDR)).
+                                Select(Function(field) String.Concat("""", field.Replace("""", """"""), """")).ToArray()
+                            OutFile.WriteLine(String.Join(",", columnValues))
+                        End While
+                    End If
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Private Shared Function DumpOneColumn(I As Integer, RDR As SqlDataReader) As String
+        If RDR.GetValue(I).GetType.FullName = "System.Byte[]" Then
+            Dim Str1 = New StringBuilder()
+            For Each One As Byte In RDR.GetValue(I)
+                Str1.Append(One.ToString("x2"))
+            Next
+            Return Str1.ToString()
+        Else
+            Return RDR.GetValue(I).ToString()
+        End If
+    End Function
+
+    Private Shared Iterator Function GetColumnNames(ByVal RDR As IDataReader) As IEnumerable(Of String)
+        For Each Row As DataRow In RDR.GetSchemaTable().Rows
+            Yield CStr(Row("ColumnName"))
+        Next
+    End Function
+End Class
---
+++ Db2CsvBackuper/Zip.vb
@@ -0,0 +1,65 @@
+﻿Imports ICSharpCode.SharpZipLib.Core
+Imports ICSharpCode.SharpZipLib.Zip
+Imports System.IO
+
+Public Class Zip
+    Public Shared Sub ZipFolder(ByVal OutPathname As String, ByVal FolderName As String)
+        If File.Exists(OutPathname) Then
+            File.Delete(OutPathname)
+        End If
+        Dim FsOut As FileStream = File.Create(OutPathname)
+        Dim ZipStream As ZipOutputStream = New ZipOutputStream(FsOut)
+        ZipStream.SetLevel(3)
+        Dim FolderOffset As Integer = FolderName.Length + (If(FolderName.EndsWith("\"), 0, 1))
+        CompressFolder(FolderName, ZipStream, FolderOffset)
+        ZipStream.IsStreamOwner = True
+        ZipStream.Close()
+    End Sub
+
+    Private Shared Sub CompressFolder(ByVal Path As String, ByVal ZipStream As ZipOutputStream, ByVal FolderOffset As Integer)
+        Dim Files As String() = Directory.GetFiles(Path)
+
+        For Each Filename As String In Files
+            Dim FI As FileInfo = New FileInfo(Filename)
+            Dim EntryName As String = Filename.Substring(FolderOffset)
+            EntryName = ZipEntry.CleanName(EntryName)
+            Dim NewEntry As ZipEntry = New ZipEntry(EntryName)
+            NewEntry.DateTime = FI.LastWriteTime
+            NewEntry.Size = FI.Length
+            ZipStream.PutNextEntry(NewEntry)
+            Dim Buffer As Byte() = New Byte(4095) {}
+
+            Using StreamReader As FileStream = File.OpenRead(Filename)
+                StreamUtils.Copy(StreamReader, ZipStream, Buffer)
+            End Using
+            ZipStream.CloseEntry()
+        Next
+
+        Dim Folders As String() = Directory.GetDirectories(Path)
+        For Each folder As String In Folders
+            CompressFolder(folder, ZipStream, FolderOffset) 'recursion
+        Next
+    End Sub
+
+    Public Shared Sub AddFileToZip(ByVal zipFile As String, ByVal fileToAdd As String)
+        If File.Exists(zipFile) Then
+            File.Delete(zipFile)
+        End If
+        Using ZipStream As ZipOutputStream = New ZipOutputStream(File.Create(zipFile))
+            Dim Buffer As Byte() = New Byte(4095) {}
+            Dim Entry As ZipEntry = New ZipEntry(System.IO.Path.GetFileName(fileToAdd))
+            Entry.DateTime = DateTime.Now
+            ZipStream.PutNextEntry(entry)
+            Using FS As FileStream = File.OpenRead(fileToAdd)
+                Dim SourceBytes As Integer = 0
+                Do
+                    SourceBytes = FS.Read(Buffer, 0, Buffer.Length)
+                    ZipStream.Write(Buffer, 0, SourceBytes)
+                Loop While SourceBytes > 0
+            End Using
+            ZipStream.Finish()
+            ZipStream.Close()
+            ZipStream.Dispose()
+        End Using
+    End Sub
+End Class
